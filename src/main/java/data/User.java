76ac99655ca86b832d84f0361bfc18273d96666c
Yuliya Chirkova <chirkovay2014@gmail.com>
2021-09-05 23:05:32 +0300

    init commit

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -2,11 +2,11 @@ package data;
 
 public class User {
 
-    public String userName ="Admin";// "TomFord"; //"Chris Northon";
-    public String password = "admin123";//"TomFord1"; //"12345678";
-    public String employeeName ="Alice Duval";//"Tom Ford"; //"Alice Duval"; "Aaliyah Haq";
-    public String invalidEmployeeName ="Marc Tven";
-    public String adminAddUserName = "AlicaDuva";//"Alice_Duval"; "Chris Hampton";
+    public String userName ="Admin";
+    public String password = "admin123";
+    public String employeeName ="Alice Duval";
+    public String invalidEmployeeName ="Mark Shagal";
+    public String adminAddUserName = "Al.Duv";
     public String adminAddUserPassword = "QWER1234";
     public String fromDate = "2021-11-01";
     public String toDate = "2021-11-10";
