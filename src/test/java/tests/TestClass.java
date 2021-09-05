7bfcdd103eeacb5df30a0f5d3f83bdbab6892fac
Yuliya Chirkova <chirkovay2014@gmail.com>
2021-09-05 23:20:08 +0300

    init commit

--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -1,6 +1,5 @@
 package tests;
 
-import data.Candidate;
 import data.User;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
@@ -9,9 +8,6 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import pages.*;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -19,7 +15,6 @@ import static com.codeborne.selenide.WebDriverRunner.url;
 public class TestClass extends BeforeAfterEach {
     LoginPage loginPage = new LoginPage();
     User user = new User();
-    Candidate candidate = new Candidate();
     WelcomeUserElement welcomeUserElement = new WelcomeUserElement();
     AdminTab adminTab = new AdminTab();
     RecruitmentTab recruitmentTab = new RecruitmentTab();
@@ -38,7 +33,6 @@ public class TestClass extends BeforeAfterEach {
         loginPage.openLoginPageAndLogin(user);
         welcomeUserElement.welcomeIcon.should(exist);
         Assertions.assertEquals(adminTab.mainPageURL, url(), "The incorrect page is open or invalid title is specified");
-
     }
 
     @Order(2)
@@ -57,39 +51,23 @@ public class TestClass extends BeforeAfterEach {
         adminTab.clickSaveButton();
         adminTab.checkUserExisting(user);
     }
-    @Disabled ("Включить,если тест addUserTest() провалился")
-    @Order(9)
-    @Test
-    @Description("Тест добавление пользователя для сотрудника, взятого из таблицы пользователей")
-    @Severity(SeverityLevel.NORMAL)
-    @Feature("добавление пользователя в систему")
-    @Story(value = "Сотрудник добавляет пользователя для сотрудника, взятого из таблицы пользователей")
-    public void addUserWithEmployeeFromUserListTest()  {
-        loginPage.openLoginPageAndLogin(user);
-        adminTab.selectAdminTab();
-        adminTab.addUserHeading.shouldBe(visible);
-        String employee = adminTab.getEmployeeFromUserList();
-        adminTab.clickAddButton();
-        adminTab.fillAllFieldsWithKnownEmployees(user, employee);
-        adminTab.clickSaveButton();
-        adminTab.checkUserExisting(user);
-    }
 
-    @Order(9)
+    @Order(3)
     @Test
-    @Description("Тест добавление пользователя для несуществующего сотрудника, негативный тест")
-    @Severity(SeverityLevel.NORMAL)
-    @Feature("добавление пользователя в систему")
-    //@Story(value = "Сотрудник добавляет пользователя для невалидного сщтрудника")
-    public void addUserWithInvalidEmployeeTest() {
+    @Description("Тест удаление пользователя")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Удаление пользователя")
+    @Story(value = "Сотрудник удаляет пользователя")
+    public void deleteUserTest(){
         loginPage.openLoginPageAndLogin(user);
+
         adminTab.selectAdminTab();
         adminTab.addUserHeading.shouldBe(visible);
-        adminTab.users.findBy(text(user.getInvalidEmployeeName())).shouldNot(exist);
-        adminTab.clickAddButton();
-        adminTab.invalidFillAllFields(user);
-        adminTab.clickSaveButton();
-        adminTab.errorMessage.should(exist);
+        adminTab.checkUserExisting(user);
+        adminTab.clickUserCheckBox(user);
+        adminTab.clickDeleteUserButton();
+        adminTab.confirmationRequiredOKButton.shouldBe(visible).click();
+        adminTab.checkDeleting(user);
     }
 
     @Order(4)
@@ -103,13 +81,8 @@ public class TestClass extends BeforeAfterEach {
         adminTab.getJobTitlePage();
         adminTab.fillAllFieldsForAddingJobTitle(jobTitle,jobDescription, jobNote);
         adminTab.clickSaveButton();
-      //  adminTab.jobTitles.findBy(exactText(jobTitle)).should(exist);
         adminTab.checkJobTitleIsExists(jobTitle);
         adminTab.deleteJobTitle(jobTitle);
-        adminTab.clickCheckBox(jobTitle);
-        adminTab.deleteJobTitleButton.click();
-        adminTab.confirmationRequiredOKButton.shouldBe(visible).click();
-       // adminTab.jobTitles.findBy(exactText(jobTitle)).shouldNot(exist);
         adminTab.checkJobTitleIsNotExists(jobTitle);
    }
 
@@ -166,24 +139,22 @@ public class TestClass extends BeforeAfterEach {
         welcomeUserElement.signOut();
     }
 
-    @Order(3)
+    @Order(9)
     @Test
-    @Description("Тест удаление пользователя")
-    @Severity(SeverityLevel.CRITICAL)
-    @Feature("Удаление пользователя")
-    @Story(value = "Сотрудник удаляет пользователя")
-    public void deleteUserTest(){
+    @Description("Тест добавление пользователя для несуществующего сотрудника, негативный тест")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("добавление пользователя в систему")
+    public void addUserWithInvalidEmployeeTest() {
         loginPage.openLoginPageAndLogin(user);
-
         adminTab.selectAdminTab();
         adminTab.addUserHeading.shouldBe(visible);
-        adminTab.checkUserExisting(user);
-        adminTab.clickUserCheckBox(user);
-        adminTab.clickDeleteUserButton();
-        adminTab.confirmationRequiredOKButton.shouldBe(visible).click();
-        adminTab.checkDeleting(user);
-
+        adminTab.users.findBy(text(user.getInvalidEmployeeName())).shouldNot(exist);
+        adminTab.clickAddButton();
+        adminTab.invalidFillAllFields(user);
+        adminTab.clickSaveButton();
+        adminTab.errorMessage.should(exist);
     }
+
     @Order(10)
     @Test
     @Description("Тест проверка наличия элементов в разделе Personal details во вкладке PIM")
@@ -196,4 +167,23 @@ public class TestClass extends BeforeAfterEach {
         pimTab.chooseSalesEmployee();
         pimTab.checkAllPersonalDetails();
     }
+
+    @Disabled ("Включить,если тест addUserTest() провалился")
+    @Order(11)
+    @Test
+    @Description("Тест добавление пользователя для сотрудника, взятого из таблицы пользователей")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("добавление пользователя в систему")
+    @Story(value = "Сотрудник добавляет пользователя для сотрудника, взятого из таблицы пользователей")
+    public void addUserWithEmployeeFromUserListTest()  {
+        loginPage.openLoginPageAndLogin(user);
+        adminTab.selectAdminTab();
+        adminTab.addUserHeading.shouldBe(visible);
+        String employee = adminTab.getEmployeeFromUserList();
+        adminTab.clickAddButton();
+        adminTab.fillAllFieldsWithKnownEmployees(user, employee);
+        adminTab.clickSaveButton();
+        adminTab.checkUserExisting(user);
+    }
+
 }
